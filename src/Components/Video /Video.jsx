83bc34f6ba46b17ba6f195bab0ecf36ce82d2378
Koray-afk <abhiman.singh2024@nst.rishihood.edu.in>
2025-04-28 19:29:29 +0530

    updated changes

--- src/Components/Video /Video.jsx
+++ src/Components/Video /Video.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Video.css'
-import video from "../../assets/College-video.mp4"
+
 
 function Video({playvideo,setPlayvideo}) {
   return (
     <div className='Video-media hide'>
-        <video src={video} autoPlay muted controls></video>
+        <video src='/College-video.mp4' autoPlay muted controls></video>
     </div>
   )
 }
